eaf1acede6327df91b9101527c6263f3bd0e632f
DavisLau <apple73924130@icloud.com>
2017-03-22 14:17:37 +0800

    Add files via upload

---
+++ WB/WB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WB
+//
+//  Created by 刘艳法 on 2017/3/22.
+//  Copyright © 2017年 刘艳法. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
